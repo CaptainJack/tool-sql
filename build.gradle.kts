76bd2e024d41746bd843083d2b98052df9ed176a
Alexey Chuprin <a.chuprin@capjack.ru>
2019-08-08 13:56:38 +0300

    ru.capjack.bintray 0.20.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,7 +1,7 @@
 plugins {
 	kotlin("jvm") version "1.3.41"
 	id("nebula.release") version "11.1.0"
-	id("ru.capjack.bintray") version "0.20.0"
+	id("ru.capjack.bintray") version "0.20.1"
 }
 
 group = "ru.capjack.tool"
