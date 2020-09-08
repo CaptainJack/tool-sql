bad4732559191840277ebe02b758b0d16b46c7da
Alexey Chuprin <a.chuprin@capjack.ru>
2020-09-08 16:01:58 +0300

    InClauses

--- src/main/kotlin/ru/capjack/tool/sql/AddablePreparedStatement.ext.kt
+++ src/main/kotlin/ru/capjack/tool/sql/AddablePreparedStatement.ext.kt
@@ -3,3 +3,11 @@ package ru.capjack.tool.sql
 inline fun <E : Enum<E>> AddablePreparedStatement.addEnum(x: E?, mapper: (E) -> String) {
 	setString(nextIndex(), x?.let(mapper))
 }
+
+inline fun <E> AddablePreparedStatement.addInClauses(list: Collection<E>, adder: AddablePreparedStatement.(E) -> Unit) {
+	list.forEach { this.adder(it) }
+}
+
+inline fun <E> AddablePreparedStatement.addInClauses(list: Array<E>, adder: AddablePreparedStatement.(E) -> Unit) {
+	list.forEach { this.adder(it) }
+}
