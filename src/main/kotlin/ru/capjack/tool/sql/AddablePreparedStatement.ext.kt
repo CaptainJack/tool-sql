1437766ee57eb973d6996b6b9babe216429d272c
Alexey Chuprin <a.chuprin@capjack.ru>
2020-11-16 18:21:20 +0300

    Improve addInClauses

--- src/main/kotlin/ru/capjack/tool/sql/AddablePreparedStatement.ext.kt
+++ src/main/kotlin/ru/capjack/tool/sql/AddablePreparedStatement.ext.kt
@@ -8,6 +8,38 @@ inline fun <E> AddablePreparedStatement.addInClauses(list: Collection<E>, adder:
 	list.forEach { this.adder(it) }
 }
 
-inline fun <E> AddablePreparedStatement.addInClauses(list: Array<E>, adder: AddablePreparedStatement.(E) -> Unit) {
+inline fun <E> AddablePreparedStatement.addInClauses(list: Array<out E>, adder: AddablePreparedStatement.(E) -> Unit) {
 	list.forEach { this.adder(it) }
+}
+
+
+@JvmName("addInClausesArrayInt")
+fun AddablePreparedStatement.addInClauses(list: Array<Int>) {
+	addInClauses(list, AddablePreparedStatement::addInt)
+}
+
+@JvmName("addInClausesArrayLong")
+fun AddablePreparedStatement.addInClauses(list: Array<Long>) {
+	addInClauses(list, AddablePreparedStatement::addLong)
+}
+
+@JvmName("addInClausesArrayString")
+fun AddablePreparedStatement.addInClauses(list: Array<String>) {
+	addInClauses(list, AddablePreparedStatement::addString)
+}
+
+
+@JvmName("addInClausesInt")
+fun AddablePreparedStatement.addInClauses(list: Collection<Int>) {
+	addInClauses(list, AddablePreparedStatement::addInt)
+}
+
+@JvmName("addInClausesLong")
+fun AddablePreparedStatement.addInClauses(list: Collection<Long>) {
+	addInClauses(list, AddablePreparedStatement::addLong)
+}
+
+@JvmName("addInClausesString")
+fun AddablePreparedStatement.addInClauses(list: Collection<String>) {
+	addInClauses(list, AddablePreparedStatement::addString)
 }
